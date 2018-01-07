f4f234fdb7fc2b483c302d127757b548a194e90e
Willard Joseph Linux CPU <jubilee918@WJoseph-29-26-16-box>
2018-01-07 12:03:15 -0500

    New Classes Added

---
+++ PatientInformation.h
@@ -0,0 +1,24 @@
+//===========================================================================
+// Author      : Willard D Joseph
+// FileName    : PatientInformation.h
+// Date        : Jan 5, 2018
+// Description :
+//
+//============================================================================
+#ifndef PATIENTINFORMATION_H_
+#define PATIENTINFORMATION_H_
+
+class PatientInformation {
+private:
+    std::string PatientFirstName;
+    std::string PatientLastName;
+    char PatientsInitial;
+
+public:
+
+};
+
+
+
+
+#endif /* PATIENTINFORMATION_H_ */
